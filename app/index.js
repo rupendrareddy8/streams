bf37213ddb98fb92d3aa485d596565129c5779fb
rupendra <rupendra@rupendras-MacBook-Air.local>
2021-03-20 14:39:31 +0530

    testing changes

--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-//const db = require('./config/db.js')
+const db = require('./config/db.js')
 // Routes Import
-const videoStreams = require("./videoStreams/index.js");
+//const videoStreams = require("./videoStreams/index.js");
 
 
 const router = express.Router();
 
 // Adding Routes
-router.use('/v-streams', videoStreams);
+//router.use('/v-streams', videoStreams);
 
 
 module.exports = router
